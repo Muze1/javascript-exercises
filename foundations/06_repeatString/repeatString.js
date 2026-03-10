189b7ffa48a064a047975589716c02006b1dcf16
Umair <umair.mehmood15@gmail.com>
2026-03-10 23:42:24 +0000

    Complete solution

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num >= 0) {
+        let repeatedString = string.repeat(num);
+        return console.log(repeatedString);
+    }
+    else if (num < 0) {
+        return console.log('ERROR');
+    }
 };
+
+console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
