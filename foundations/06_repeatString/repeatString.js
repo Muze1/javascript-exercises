4dc1682eae661fc72832af5a9c2a9feec8fd6532
Umair <umair.mehmood15@gmail.com>
2026-03-11 00:12:48 +0000

    Adjust condition

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,13 +1,11 @@
 const repeatString = function(string, num) {
-    if (num <= 0) return 'ERROR';
+    if (num < 0) return 'ERROR';
     let repeatedString = '';
     for (let i = 0; i < num; i++) {
         repeatedString += string;
     }
     return repeatedString;
 };
-
-console.log(repeatString('hey', 4));
 
 // Do not edit below this line
 module.exports = repeatString;
