f4465a97985f11bcb2be7f78b8313504376b5ffa
Umair <umair.mehmood15@gmail.com>
2026-03-11 08:15:08 +0000

    Complete task

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
-};
+// const removeFromArray = function(array, ...args) {
+//     const newArray = [];
+
+//     array.forEach((item) => {
+//         if(!args.includes(item)) {
+//             newArray.push(item);
+//         }
+//     })
+
+//     return newArray;
+// };
+
+const removeFromArray = function(array, ...args) {
+    return array.filter(val => !args.includes(val))
+}
+
+console.log(removeFromArray([1, 2, 3], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// 1. take 2 arguments, array and num
+// 2. iterate through each item of array
+// 3. check if item of array is equal to num
+// 4. if no, leave alone
+// 5. if yes, remove that item from array
