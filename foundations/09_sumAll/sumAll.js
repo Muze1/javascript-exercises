096cbec4813b15a50449698dd471e1a2e58dae0e
Umair <umair.mehmood15@gmail.com>
2026-03-14 16:33:20 +0000

    Complete solution and add efficient one

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -2,16 +2,29 @@ const sumAll = function(a, b) {
     let c = 0;
     let total = 0;
 
-    if (a > b) { 
+
+    if ((Number.isInteger(a) === false) ||
+    (Number.isInteger(b) === false) ||
+    (a < 0) ||
+    (b < 0)) {
+        return 'ERROR'; 
+    } else if (a > b) { 
         c = a;
         a = b;
         b = c;
-     }
+    }
 
-     for (let i = a; i <= b; i++) {
-        
-     }
+    for (let i = a; i <= b; i++) {
+        total += i;
+    }
+    return total;
+
+    // // Efficient solution
+    // if (!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
+    // if (a < 0 || b < 0) return 'ERROR';
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
